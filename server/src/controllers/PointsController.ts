32877ba512f22eeffa2f2fbb162d468a1e1d657d
LucasEdu10 <lucas.carvalho.luz1@gmail.com>
2020-06-07 17:37:09 -0300

    Ajuste no back-end

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -17,7 +17,7 @@ class PoinstController{
         const trx = await knex.transaction();
     
         const point = {
-            image: 'https://images.unsplash.com/photo-1578916171728-46686eac8d58?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
+            image: request.file.filename,
             name,
             email,
             whatsapp,
@@ -31,7 +31,7 @@ class PoinstController{
     
         const point_id = insertedIds[0];
       
-        const pointsItems = items.map((item_id: number) => {
+        const pointsItems = items.split(',').map((item: string) => Number(item.trim())).map((item_id: number) => {
             return{
                 item_id,
                 point_id,
@@ -50,16 +50,23 @@ class PoinstController{
     async show(request: Request, response: Response) {
         const {id} = request.params;
         const point = await knex('points').where('id', id).first();
+
         if(!point){
             return response.status(400).json({message:'Point not found.'});
         }
+
+        const serilizedPoint = {
+            ...point,
+            image_url: `http://192.168.1.4:3333/uploads/${point.image}`
+        };
 
         const items = await knex('items')
         .join('point_items', 'items.id', '=', 'point_items.item_id')
         .where('point_items.point_id', id).select('items.title');
 
-        return response.json({point, items});
+        return response.json({point: serilizedPoint, items});
     }
+    
     async index(request: Request, response: Response){
         const {city, uf, items} = request.query;
 
@@ -74,7 +81,14 @@ class PoinstController{
         .distinct()
         .select('points.*');
 
-        return response.json(points);
+        const serilizedPoints = points.map(point =>{
+            return {
+                ...point,
+                image_url: `http://192.168.1.4:3333/uploads/${point.image}`
+            };
+        });
+
+        return response.json(serilizedPoints);
     }
 }
 
